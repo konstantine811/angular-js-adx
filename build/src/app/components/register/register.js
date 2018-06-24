cce60e36cdca4451cbfb8fc542a129edba1050dc
Vahid Kowsari <vahid@kowsari.com>
2018-06-24 10:34:39 -0700

    Fix register with token function

--- build/src/app/components/register/register.js
+++ build/src/app/components/register/register.js
@@ -5,29 +5,39 @@ angular.module( 'ixlayer.register', [
   'ngAnimate'
 ])
   .config(function config( $stateProvider ) {
+    $stateProvider.state( 'master.register_with_key', {
+      url: '/register/:activation_key',
+      controller: 'RegisterCtrl',
+      templateUrl: 'components/register/register.tpl.html',
+      data:{ pageTitle: 'Register' }
+    });
     $stateProvider.state( 'master.register', {
-      url: '/register',
+      url: '/register/:token',
       controller: 'RegisterCtrl',
       templateUrl: 'components/register/register.tpl.html',
       data:{ pageTitle: 'Register' },
-      params: {
-        complete: false,
-        errors: null
-      }
     });
   })
 
-  .controller( 'RegisterCtrl', ['$scope', 'userAccessSrv',  function RegisterCtrl($scope, userAccessSrv) {
+  .controller( 'RegisterCtrl', ['$scope', 'userAccessSrv', '$stateParams',
+    function RegisterCtrl($scope, userAccessSrv, $stateParams) {
     $scope.complete = false;
     $scope.error = null;
     $scope.showPopup = false;
     $scope.notChecked = false;
 
+    $scope.hasToken = $stateParams.activation_key !== "";
+    $scope.token = $scope.hasToken? $stateParams.activation_key : null;
+
     $scope.submitForm = function (form) {
       $scope.showPopup = true;
       $scope.submitConsent = function() {
         $scope.showPopup = false;
-        userAccessSrv.register(form.first_name, form.last_name, form.email, form.password,  {'consent': 1, 'terms': 1}).then(function (result) {
+        userAccessSrv.register(form.first_name, form.last_name, form.email, form.password,  {
+          'consent': 1,
+          'terms': 1,
+          'token': $scope.token
+        } ).then(function (result) {
           $scope.complete = true;
         }, function (error) {
             if (error.status === 0) {
