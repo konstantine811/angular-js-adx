f553124a63f7e0c3eae336f1a2dff318631f3771
Vahid Kowsari <vahid@kowsari.com>
2018-06-28 14:35:14 -0700

    Added includes polyfill for IE 11

--- build/assets/javascript/polyfills.js
+++ build/assets/javascript/polyfills.js
@@ -78,3 +78,17 @@ if (!Array.prototype.find) {
     writable: true
   });
 }
+
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    if (typeof start !== 'number') {
+      start = 0;
+    }
+
+    if (start + search.length > this.length) {
+      return false;
+    } else {
+      return this.indexOf(search, start) !== -1;
+    }
+  };
+}
