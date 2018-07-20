d2e47733188eb6c9c2199f441d139c52aa2b0f54
Vahid Kowsari <vahid@kowsari.com>
2018-07-20 12:16:03 -0700

    IXLAYER-57 Logout before verifying email

--- build/src/app/components/verify_email/verify_email.js
+++ build/src/app/components/verify_email/verify_email.js
@@ -9,11 +9,18 @@ angular.module( 'ixlayer.verifyemail', [
       url: '/verify-email/:activation_key',
       controller: 'VerifyEmailCtrl',
       templateUrl: 'components/verify_email/verify_email.tpl.html',
-      data:{ pageTitle: 'Verify Email' }
+      data:{ pageTitle: 'Verify Email' },
+      resolve: {
+        cleanUser: ['userAccessSrv', function (userAccessSrv) {
+            return userAccessSrv.cleanUser();
+          }]
+      }
     });
   })
 
-  .controller( 'VerifyEmailCtrl', ['$scope', '$stateParams', 'userAccessSrv', function VerifyEmailCtrl( $scope, $stateParams, userAccessSrv ) {
+  .controller( 'VerifyEmailCtrl', ['$scope', '$stateParams', 'userAccessSrv',
+    function VerifyEmailCtrl( $scope, $stateParams, userAccessSrv ) {
+
     $scope.complete = false;
 
     userAccessSrv.verifyEmail($stateParams.activation_key).then(function (result) {
